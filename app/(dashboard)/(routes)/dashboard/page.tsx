d4076be222118603c5472912da2ad5c3fe5fe969
MuchCauliflower <mharmikhail.canlas@gmail.com>
2026-01-14 21:28:12 +0800

    Landing page (1): adjust inline spacing (mb to pb)

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -63,7 +63,7 @@ export default function DashboardPage() {
   const router = useRouter();
   return (
     <div>
-      <div className="mb-8 space-y-4">
+      <div className="pb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">Gyaru</h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           GyaruGyaru smartest AI - Experience the power of Gyaru

b8ac0e16b5795cd8132346ea861862a431fd2e08
MuchCauliflower <mharmikhail.canlas@gmail.com>
2026-01-14 13:25:41 +0800

    Inital Commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard (Protected)</h1>
+      <Button variant="destructive">Click Me</Button>
+    </div>
+  );
+}

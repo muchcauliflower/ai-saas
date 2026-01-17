a8a891fc423ff55cfb073f757eac543ce1ef03b6
MuchCauliflower <mharmikhail.canlas@gmail.com>
2026-01-17 11:38:49 +0800

    Fixed Layout for Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,14 +1,18 @@
+import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import Heroes from "./_components/heroes";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center text-center gap-y-8 flex-1 px-6 pb-10">
+    <div className="h-full flex flex-col">
+      <div className="flex-1 flex flex-col items-center justify-center gap-y-8 px-6 text-center">
         <Heading />
         <Heroes />
       </div>
+
+      <Footer />
     </div>
   );
 };
+
 export default LandingPage;

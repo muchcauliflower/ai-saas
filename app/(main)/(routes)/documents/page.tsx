7a1d92ebc12b2ceaff6a20213968344e0a2e8e87
MuchCauliflower <mharmikhail.canlas@gmail.com>
2026-01-28 10:14:12 +0800

    feat: Added content for Document's Page - ModeToggle - simple Welcome page with SVGs

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,42 @@
 "use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import { ModeToggle } from "@/components/mode-toggle";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      <h1>Documents Page Protected</h1>
+    <div className="h-full">
+      <div className="flex justify-end p-4">
+        <ModeToggle />
+      </div>
+      <div className="h-full flex flex-col items-center justify-center space-y-4 mt-[-70px]">
+        <Image
+          src="/document-empty.svg"
+          height="300"
+          width="300"
+          alt="Empty"
+          className="dark:hidden"
+        />
+        <Image
+          src="/document-empty-dark.svg"
+          height="300"
+          width="300"
+          alt="Empty"
+          className="hidden dark:block"
+        />
+        <h2 className="text-lg font-medium">
+          Welcome to {user?.firstName}&apos;s Documents Page!
+        </h2>
+        <Button>
+          <PlusCircle className="h-2 w-4 mr-2" />
+          Create a note
+        </Button>
+      </div>
     </div>
   );
 };

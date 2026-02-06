e42cf262d24ca8f4d1105fe369db2132230f5b72
MuchCauliflower <mharmikhail.canlas@gmail.com>
2026-02-06 14:31:18 +0800

    feat: enhance DocumentIdPage with document fetching; add Toolbar and IconPicker components for improved document management

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,47 @@
-const DocumentIdPage = () => {
-  return <div>DocumentId</div>;
+"use client";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import React from "react";
+
+interface DocumentIdPageProps {
+  params: Promise<{
+    documentId: Id<"documents">;
+  }>;
+}
+
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+  const { documentId } = React.use(params);
+
+  const document = useQuery(api.documents.getById, {
+    documentId
+  });
+  
+  
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  };
+
+  if (document === null) {
+    return <div>Not found</div>
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]"/>
+      <div className="md:max-w-3xl lg:max-w-4xl mx-4.5">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
